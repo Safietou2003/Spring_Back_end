51bcd81337467a373b4b77e107addab40e185602
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 13:42:18 +0000

    Update AuthServiceImpl.java

--- src/main/java/com/ges_abs/services/impl/AuthServiceImpl.java
+++ src/main/java/com/ges_abs/services/impl/AuthServiceImpl.java
@@ -1,1 +1,23 @@
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Map<String, Object> login(String login, String password) {
+        Optional<User> userOpt = userRepository.findByUsernameAndPassword(login, password);
+
+        Map<String, Object> response = new HashMap<>();
+        if (userOpt.isPresent()) {
+            response.put("success", true);
+            response.put("user", userOpt.get());
+        } else {
+            response.put("success", false);
+            response.put("message", "Identifiants incorrects");
+        }
+
+        return response;
+    }
+}
 
