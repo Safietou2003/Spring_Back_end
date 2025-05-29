35121fdc1810751096a0484b779f4ed79e4af7cd
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 13:53:43 +0000

    Update AbsenceRepository.java

--- src/main/java/com/ges_abs/data/repository/AbsenceRepository.java
+++ src/main/java/com/ges_abs/data/repository/AbsenceRepository.java
@@ -19,5 +19,5 @@ public interface AbsenceRepository extends MongoRepository<Evenement, String> {
     Optional<Evenement> findById(long id);
     Page<Evenement> findByMatricule(String matricule, Pageable pageable);
     List<Evenement> findAll();
-    List<Absence> findByMatricule(String matricule);
+    List<Evenement> findByMatricule(String matricule);
 }
