4f12301394d00f88a0fd39fbfee9684d1094e8f4
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 15:06:05 +0000

    Update CoursServiceImpl.java

--- src/main/java/com/ges_abs/services/impl/CoursServiceImpl.java
+++ src/main/java/com/ges_abs/services/impl/CoursServiceImpl.java
@@ -5,4 +5,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CoursServiceImpl implements CoursService {
+
+    @Autowired
+    private CoursRepository coursRepository;
+
+    @Override
+    public Cours save(Cours cours) {
+        return coursRepository.save(cours);
+    }
+
+    @Override
+    public List<Cours> findAll() {
+        return coursRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cours> findById(String id) {
+        return coursRepository.findById(id);
+    }
+
+    @Override
+    public void delete(String id) {
+        coursRepository.deleteById(id);
+    }
 }
