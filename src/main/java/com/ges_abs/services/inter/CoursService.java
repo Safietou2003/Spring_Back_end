a56ccc964ca5093e870553985e33bf44875b6107
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 15:05:40 +0000

    Update CoursService.java

--- src/main/java/com/ges_abs/services/inter/CoursService.java
+++ src/main/java/com/ges_abs/services/inter/CoursService.java
@@ -1,4 +1,8 @@
 package com.ges_abs.services.inter;
 
 public interface CoursService {
+    Cours save(Cours cours);
+    List<Cours> findAll();
+    Optional<Cours> findById(String id);
+    void delete(String id);
 }
