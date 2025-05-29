a5b92b85b5a34c8040a80ee54075b646095178e7
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 15:38:10 +0000

    Update CoursController.java

--- src/main/java/com/ges_abs/mobile/controller/inter/CoursController.java
+++ src/main/java/com/ges_abs/mobile/controller/inter/CoursController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.Map;
 
