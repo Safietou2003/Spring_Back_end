d5adef2933f0907d4b6780222987bde01441360d
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 15:13:11 +0000

    Update PointageService.java

--- src/main/java/com/ges_abs/services/inter/PointageService.java
+++ src/main/java/com/ges_abs/services/inter/PointageService.java
@@ -1,4 +1,7 @@
 package com.ges_abs.services.inter;
 
 public interface PointageService {
+    ResponseEntity<?> pointerEtudiant(String matricule, String sessionId);
+    List<Pointage> getAll();
 }
+
