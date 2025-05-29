553c5ad04d9b911c1e22e042487e31c76b81259a
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 15:40:53 +0000

    Update AbsenceController.java

--- src/main/java/com/ges_abs/mobile/controller/inter/AbsenceController.java
+++ src/main/java/com/ges_abs/mobile/controller/inter/AbsenceController.java
@@ -1,3 +1,16 @@
+package com.ges_abs.mobile.controller.inter;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.Map;
+
 @RestController
 @RequestMapping("/absences")
 public class AbsenceController {
