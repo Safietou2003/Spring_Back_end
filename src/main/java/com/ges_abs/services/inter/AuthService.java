5a61a7abbd8c0314fa29899eaa298b9de6a2e6aa
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-29 13:39:29 +0000

    Update AuthService.java

--- src/main/java/com/ges_abs/services/inter/AuthService.java
+++ src/main/java/com/ges_abs/services/inter/AuthService.java
@@ -1,1 +1,15 @@
+package com.ges_abs.services.inter;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.ges_abs.data.models.entity.Evenement;
+import com.ges_abs.data.models.enumeration.Etat;
+import com.ges_abs.data.models.enumeration.Type;
+
+import java.util.List;
+
+public interface AuthService {
+    Map<String, Object> login(String username, String password);
+}
 
